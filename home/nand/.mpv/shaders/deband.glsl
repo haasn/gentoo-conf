423f9f30df6b1eb48a82451a8037cdd5f02b7f35
Niklas Haas <git@nand.wakku.to>
2015-03-27 16:58:32 +0100

    2015-03-27: improve deband.glsl

--- home/nand/.mpv/shaders/deband.glsl
+++ home/nand/.mpv/shaders/deband.glsl
@@ -12,21 +12,27 @@ float rand(vec2 co){
 
 vec4 sample(sampler2D tex, vec2 pos, vec2 size, vec2 sub)
 {
-    vec2 pt = 1.0 / (size * sub);
-    float d = rand(pos + pt) * range;
+    // Compute a random angle and distance
+    float dist = rand(pos + 5/size) * range;
+    vec2 pt = dist / (size * sub);
+    float turn = 1.570796;
+    float dir = rand(pos - 7/size) * 4*turn;
 
+    // Sample at quarter-turn intervals around the source pixel
     vec4 ref[4];
-    ref[0] = texture(tex, pos + pt * vec2( d, 0));
-    ref[1] = texture(tex, pos + pt * vec2( 0, d));
-    ref[2] = texture(tex, pos + pt * vec2(-d, 0));
-    ref[3] = texture(tex, pos + pt * vec2( 0,-d));
+    for (int n = 0; n < 4; n++)
+        ref[n] = texture(tex, pos + pt * vec2(cos(dir+n*turn),sin(dir+n*turn)));
 
+    // Average and compare with the actual sample
     vec4 avg = (ref[0] + ref[1] + ref[2] + ref[3])/4.0;
     vec4 col = texture(tex, pos);
     vec4 diff = abs(col - avg);
 
+    // Use the average if below the threshold
     col = mix(avg, col, greaterThan(diff, vec4(threshold/16384.0)));
+
+    // Add some random noise to each channel
     col.rgb += (grain/8192.0) *
-        (vec3(rand(pos), rand(pos+vec2(d)), rand(pos-vec2(d))) - vec3(0.5));
+        (vec3(rand(pos), rand(pos+vec2(3*dir)), rand(pos-vec2(11*dist))) - vec3(0.5));
     return col;
 }
