9feee2202f6221c4a26b628ccbec3d33bacea799
nand <nand@lavabit.com>
2015-08-16 13:04:30 +0200

    2015-08-16: routing skype via valkyrja

--- etc/openvpn/valkyrja/up.sh
+++ etc/openvpn/valkyrja/up.sh
@@ -1,2 +1,5 @@
 #!/bin/dash
 route add -net 198.41.128.0/17 gw 10.60.0.1
+ip route add default via 10.60.0.1 dev tap1 table valk-route
+ip rule add fwmark 0x2 table valk-route
+sysctl net.ipv4.conf.tap1.rp_filter=2
