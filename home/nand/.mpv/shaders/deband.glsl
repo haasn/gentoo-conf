0fd219c98a82884881e12d021b6d486d16c204de
nand <nand@lavabit.com>
2015-08-19 16:35:40 +0200

    deband.glsl: improve with higher-quality PRNG

--- home/nand/.mpv/shaders/deband.glsl
+++ home/nand/.mpv/shaders/deband.glsl
@@ -6,15 +6,22 @@
 #define range     16
 #define grain     24
 
-float rand(vec2 co) {
-    return fract(sin(dot(co.yx, vec2(12.9898,78.233))) * 43758.5453);
-}
+// decent-quality PRNG, shamelessly stolen from a GLSL tricks forum post
+float mod289(float x)   { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+float permute(float x)  { return mod289(((x*34.0)+1.0)*x); }
+float rand(float x)     { return fract(x*1.0/41.0); }
 
 vec4 sample(sampler2D tex, vec2 pos, vec2 tex_size)
 {
+    float h;
+    // Initialize the PRNG by hashing the position + a random uniform
+    vec3 m = vec3(pos, random) + vec3(1.0);
+    h = permute(permute(permute(m.x)+m.y)+m.z);
+
     // Compute a random angle and distance
-    float dist = rand(pos + vec2(random)) * range;
-    float dir  = rand(pos.yx - vec2(random)) * 6.2831853;
+    float dist = rand(h) * range;     h = permute(h);
+    float dir  = rand(h) * 6.2831853; h = permute(h);
+
     vec2 pt = dist / image_size;
     vec2 o = vec2(cos(dir), sin(dir));
 
@@ -34,9 +41,11 @@ vec4 sample(sampler2D tex, vec2 pos, vec2 tex_size)
     col = mix(avg, col, greaterThan(diff, vec4(threshold/16384.0)));
 
     // Add some random noise to the output
-    vec3 noise = vec3(rand(2*pos + vec2(random)),
-                      rand(3*pos + vec2(random)),
-                      rand(4*pos + vec2(random)));
+    vec3 noise;
+    noise.x = rand(h); h = permute(h);
+    noise.y = rand(h); h = permute(h);
+    noise.z = rand(h); h = permute(h);
+
     col.rgb += (grain/8192.0) * (noise - vec3(0.5));
     return col;
 }
